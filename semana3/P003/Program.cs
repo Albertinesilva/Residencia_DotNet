86404ec94cdfeb854cf049464889d3f9838e6e08
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-29 20:37:35 -0300

    Atualização da Main

--- semana3/P003/Program.cs
+++ semana3/P003/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace P003
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      EstoqueService estoque = new EstoqueService();
+      App.MenuEstoque(estoque);
+    }
+  }
+}
